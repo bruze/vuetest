ccb25f609e11fd94dda59aa208c5f406156c229e
Bruno Garelli <bgarelli@Altimetriks-MacBook-Pro.local>
2018-12-24 10:28:37 -0300

    First steps

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueHoodie from 'vue-hoodie'
+
+import Hoodie from '@hoodie/client'
+import PouchDB from 'pouchdb';
+
+Vue.use(VueHoodie);
 
 Vue.config.productionTip = false
 
+const hoodie = new Hoodie({
+  PouchDB,
+  url: 'http://localhost:8080'
+});
+
+
+hoodie.store.add({
+    type: 'todo-item',
+    content: 'Try out hoodie!',
+    done: false
+})
+
 new Vue({
   render: h => h(App),
+  hoodie,
 }).$mount('#app')
